a6a55836333c76a5a0a8334cd36102a643ced771
jungjaehyun <jungjaehyun@192.168.0.9>
2012-11-08 00:56:03 +0900

    Update_121108

--- cafebnb/app/view/CafeV.js
+++ cafebnb/app/view/CafeV.js
@@ -21,18 +21,20 @@ Ext.define("cafe.view.CafeV", {
 				items:[{
 					text:"Popular",
 					//flex:5,
-					width:120
+					width:120,
+					pressed:true,
 				},{
 					text:"Cafebnb picks",
 					width:120,
+					
 				}]
 			}]
 		},{
 			xtype:"list",
 			store:"cafeS",
+			style:"background-color:grey",
 			scroll:"vertical",
-			itemTpl: new Ext.Template(["<img src='{picture}.png' width='300', height='150'>",]),
-			
+			itemTpl: new Ext.Template(["<div style=\"background:url('{picture}.png'); width:300px ;height:180px;text-align:center\"><div style=\"padding: 155px 0px 2px 180px;\"><font color=white>{name}</font></div></div>",]),			
 			emptyText:"데이터가 아직 없습니다",
 	}]
 		
